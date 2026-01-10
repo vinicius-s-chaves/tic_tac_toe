1b4ea12e011a811acae087da1bfa5bfd7ff6f448
Vinicius Chaves <228245525+vinicius-s-chaves@users.noreply.github.com>
2026-01-10 11:07:15 -0300

    Add round functionality to Game class. It allows each player to choose a space and fill it in the board with their own char

--- lib/game.rb
+++ lib/game.rb
@@ -3,19 +3,38 @@ require_relative 'game/player'
 
 class Game
   attr_accessor :p1, :p2
-
+  
   def self.start
     @@status = 'in_game'
 
-    @p1 = Player.new(self.choose)
-    @p2 = Player.new(self.choose)
+    @p1 = Player.new(self.set_char)
+    @p2 = Player.new(self.set_char)
+
+    while @@status == 'in_game'
+      self.round
+    end
   end
 
   private
 
-  def self.choose
-      puts "Choose your char (x/o)"
-      char = gets.chomp.downcase
-      char
+  def self.set_char
+    puts "Choose your char (x/o)"
+    char = gets.chomp.downcase
+    char
+  end
+
+  def self.round
+    print "Player 1: "
+    Board.play(self.space, @p1)
+
+    print "Player 2: "
+    Board.play(self.space, @p2)
+  end
+  
+  def self.space
+    puts "Choose a space:"
+    p Board.display
+    space = gets.chomp
+    space
   end
 end
